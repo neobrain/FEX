353825742f92d3b8c8347fa57f66c865e4ac5003
Tony Wasserka <neobrain@users.noreply.github.com>
2025-02-16 12:01:22 +0100

    FEXLinuxTests: Allow using clang for cross-compiling

--- toolchain_x86_64.cmake
+++ toolchain_x86_64.cmake
@@ -1,9 +1,9 @@
-option(ENABLE_CLANG_THUNKS "Enable building thunks with clang" FALSE)
+option(ENABLE_CLANG_CROSSCOMPILING "Use clang for cross-compiling" TRUE)
 
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-if (ENABLE_CLANG_THUNKS)
-  message(STATUS "Enabling thunk clang building. Force enabling LLD as well")
+if (ENABLE_CLANG_CROSSCOMPILING)
+  message(STATUS "Using clang for cross-compiling. Force enabling LLD as well")
 
   set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
   set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
