4ff4fdcfad4c1a7a25a4c511ba7e54a00cb98cd2
Tony Wasserka <neobrain@users.noreply.github.com>
2025-02-19 21:41:45 +0100

    ARM64Emitter: Force NOP padding to be enabled
    This makes code caching bringup easier for the time being.

--- FEXCore/Source/Interface/Core/JIT/Arm64Relocations.cpp
+++ FEXCore/Source/Interface/Core/JIT/Arm64Relocations.cpp
@@ -33,7 +33,7 @@ void Arm64JITCore::InsertNamedThunkRelocation(ARMEmitter::Register Reg, const IR
 
   uint64_t Pointer = reinterpret_cast<uint64_t>(EmitterCTX->ThunkHandler->LookupThunk(Sum));
 
-  LoadConstant(ARMEmitter::Size::i64Bit, Reg, Pointer, EmitterCTX->Config.CacheObjectCodeCompilation());
+  LoadConstant(ARMEmitter::Size::i64Bit, Reg, Pointer, /*EmitterCTX->Config.CacheObjectCodeCompilation()*/ true);
   Relocations.emplace_back(MoveABI);
 }
 
